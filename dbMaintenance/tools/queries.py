896cc720ca17de22f46a575468860d4197ba65d7
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-09-14 18:47:46 -0400

    Updated to include queries for mouse, experiments, reviewers, participant_details. Participant_details still has one query to refactor in this file

--- dbMaintenance/tools/queries.py
+++ dbMaintenance/tools/queries.py
@@ -25,3 +25,28 @@ def list_all_reviewer_ids(cursor):
     cursor.execute("SELECT reviewer_id FROM reviewers;")
     return list(item for tup in cursor.fetchall() for item in tup)
 
+
+def list_all_detail_ids(cursor):
+    cursor.execute("SELECT detail_id FROM participant_details;")
+    return util.list_from_cursor(cursor.fetchall())
+
+# def list_participants_for_experiment(experiment_name, testing=False, postgresql=None):
+#
+#     def list_participants_main(a_cursor, exp_id):
+#         a_cursor.execute("SELECT mouse_id FROM all_participants_all_experiments WHERE experiment_id = %s;",
+#                          (exp_id,))
+#         return util.list_from_cursor(cursor.fetchall())
+#
+#     experiment_id = Experiment.get_id(experiment_name, testing, postgresql)
+#     if len(experiment_id) == 1:
+#         experiment_id = experiment_id[0]
+#     else:
+#         warnings.warn("Multiple experiments in the database with the same name.")
+#
+#     if testing:
+#         with TestingCursor(postgresql) as cursor:
+#             return list_participants_main(cursor, experiment_id)
+#     else:
+#         with Cursor() as cursor:
+#             return list_participants_main(cursor, experiment_id)
+
