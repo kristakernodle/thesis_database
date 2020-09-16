80057bcb452fa9c81b0c1e4cf2b4d2f90c35988d
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-09-16 19:18:39 -0400

    Updated filenames to prevent confusion with specific function names

--- dbMaintenance/__init__.py
+++ dbMaintenance/__init__.py
@@ -1,4 +1,4 @@
 
 from .setup_database import setup_database
-from .create_all_tables import create_all_tables_main as create_all_tables
-from .create_all_views import create_views_main as create_all_views
+from .create_tables import create_all_tables_main as create_all_tables
+from .create_views import create_views_main as create_all_views
