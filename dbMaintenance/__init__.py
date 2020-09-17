4f82731ec5b4c37c172518f95949030c2f4ae427
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-09-17 12:48:11 -0400

    Update database from directories added

--- dbMaintenance/__init__.py
+++ dbMaintenance/__init__.py
@@ -2,3 +2,4 @@
 from .setup_database import setup_database
 from .create_tables import create_all_tables_main as create_all_tables
 from .create_views import create_views_main as create_all_views
+from .update_database import update_from_data_dirs
---
+++ dbMaintenance/update_database.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import models
+
+
+def update_from_data_dirs(experiment):
+    if not Path(experiment.experiment_dir).exists():
+        print(f'Experiment directory, {experiment.experiment_dir}, does not exist.')
+
+    all_participant_dirs = list(Path(experiment.experiment_dir).glob('et*/'))
+
+    for participant_dir in all_participant_dirs:
+
+        eartag_num = int(participant_dir.name.strip('et'))
+        mouse = models.Mouse.from_db(eartag_num)
+
+        if mouse is None:
+            continue
+
+        mouse_details = models.ParticipantDetails.from_db(eartag_num, experiment.experiment_name)
+
+        if mouse_details is None:
+            continue
+
+        if experiment.experiment_name == 'skilled-reaching':
+            training_dir = Path(mouse_details.participant_dir).joinpath('Training')
+            all_session_dirs = training_dir.glob(f'et{eartag_num}_*_*T*/')
+        elif experiment.experiment_name == 'grooming':
+            training_dir = Path(mouse_details.participant_dir)
+            all_session_dirs = training_dir.glob(f'et{eartag_num}_*_*G*/')
+        else:
+            all_session_dirs = None
+            print('Need information about training and session directories for this experiment')
+
+        for session_dir in all_session_dirs:
+            session_date = int(session_dir.name.split('_')[1])
+            session = models.Session(mouse.mouse_id, experiment.experiment_id,
+                                     str(session_dir), session_date).save_to_db()
+
+            if experiment.experiment_name == 'skilled-reaching':
+                all_folder_dirs = session_dir.glob('Reaches*')
+            elif experiment.experiment_name == 'grooming':
+                print('Need information about how to input grooming experiment from data dirs')
+                continue
+            else:
+                all_folder_dirs = None
+                print('Need information about folder and trial directories for this experiment')
+
+            for folder_dir in all_folder_dirs:
+                folder = models.Folder(session.session_id, str(folder_dir)).save_to_db()
+                all_trial_dirs = folder_dir.glob('*R*.mp4')
+
+                if len(list(all_trial_dirs)) == 0:
+                    all_trial_dirs = folder_dir.glob('*R*.MP4')
+
+                for trial_dir in all_trial_dirs:
+                    models.Trial(experiment.experiment_id,
+                                 folder.folder_id,
+                                 str(trial_dir),
+                                 session.session_date).save_to_db()
